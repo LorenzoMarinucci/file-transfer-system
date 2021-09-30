5f1b1a59a08a49540fd756e82c1aeeede04e1ece
julyvjo <julyvjo@gmail.com>
2021-09-29 23:42:47 -0300

    iniciando server con express, devolviendo el client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8080;
 
+/*
+esto es por si usamos 'express pug' para renderizar dinamicamente el html en un futuro con res.render()
+app.set('views','./views');
+app.set('view engine', 'pug');
+*/
+
 app.get('/', (req, res) => {
-  res.send('Torrente server!');
+  res.sendFile(path.join(__dirname, '../client', 'index.html')); //envia el index.html del directorio client cuando se hace un GET a '/'
 })
 
 app.listen(port, () => {
