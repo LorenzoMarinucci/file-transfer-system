845ad42b7bebb84ae2455767d4d262cce2ff7a1e
LuPalomeque <lucialourdespalomeque@gmail.com>
2021-10-09 19:33:49 -0300

    agregado de ventana de carga de archivos
    y cambios miscelaneos

--- client/index.js
+++ client/index.js
@@ -1,18 +1,47 @@
-const URL_FILES = 'http://localhost:8080/files';
+const URL_FILES = "http://localhost:8080/files";
 
-const ListResponse = document.querySelector('#files-list');
+const ListResponse = document.querySelector("#files-list");
 
 fetch(URL_FILES)
-    .then((response) => response.json())
-    .then((value) => {
-        let tpl = '';
-        Object.entries(value).map(entry => {
-            let hash = entry[0];
-            let file = entry[1];
-            let filename = file.filename;
-            let filesize = file.filesize;
-            let par = file.par;
-            tpl += `<li> ${hash} ${filename} ${filesize} ${par}</li>`;
-        });
-        ListResponse.innerHTML = `<ul>${tpl}</ul>`;
-    })
+  .then((response) => response.json())
+  .then((value) => {
+    let tpl = "";
+    Object.entries(value).map((entry) => {
+      let hash = entry[0];
+      let file = entry[1];
+      let filename = file.filename;
+      let filesize = file.filesize;
+      let par = file.par;
+      tpl += `<li> ${hash} ${filename} ${filesize} ${par}</li>`;
+    });
+    ListResponse.innerHTML = `<ul>${tpl}</ul>`;
+  });
+
+var Alert = new CustomAlert();
+
+function CustomAlert() {
+  this.render = function () {
+    //Show Modal
+    let popUpBox = document.getElementById("popUpBox");
+    popUpBox.style.display = "block";
+    //Close Modal
+    document.getElementById("closeModal").innerHTML =
+      '<button onclick="Alert.ok()">Cargar</button>';
+    document.getElementById("file-name").innerHTML = "Nombre del archivo: ";
+    document.getElementById("file-size").innerHTML = "Tamaño: ";
+  };
+  this.ok = function () {
+    document.getElementById("popUpBox").style.display = "none";
+    document.getElementById("popUpOverlay").style.display = "none";
+  };
+}
+
+document.getElementById("file-input").addEventListener("change", (event) => {
+  var files = event.target.files,
+    file = files[0];
+
+  document.getElementById("file-name").innerHTML =
+    "Nombre del archivo: " + file.name;
+  document.getElementById("file-size").innerHTML =
+    "Tamaño: " + file.size + " bytes";
+});
