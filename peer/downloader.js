d833494ef4f70376b1f1ba966f5938a75ca61df9
LorenzoMarinucci <lorenmarinucci@gmail.com>
2021-10-30 16:30:51 -0300

    Descargar archivo entre pares

--- peer/downloader.js
+++ peer/downloader.js
@@ -1,18 +1,63 @@
-var net = require('net');
-//const TCP_HOST = 123; estos valores los devolveria el tracker
-//const TCP_PORT = 123;
+let net = require("net");
+let fs = require("fs");
+const FILES_DIR = "./files/";
 
-var client = new net.Socket();
+async function startDownload(filename, peerIp, peerPort) {
+  const filePath = FILES_DIR + filename;
 
-client.connect(TCP_PORT, TCP_HOST, function() {
-	console.log('Connected to ', TCP_HOST);
-});
+  await fs.unlink(filePath, (err) => {}); // borra el archivo previo
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy();
-});
+  let client = new net.Socket();
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+  client.connect(peerPort, peerIp, function () {
+    console.log("Connected to ", peerIp);
+  });
+
+  let ostream = fs.createWriteStream("./files/testvuelta.txt");
+  let date = new Date(),
+    size = 0,
+    elapsed;
+
+  client.on("data", (chunk) => {
+    size += chunk.length;
+    elapsed = new Date() - date;
+    client.write(
+      `\r${(size / (1024 * 1024)).toFixed(
+        2
+      )} MB of data was sent. Total elapsed time is ${elapsed / 1000} s`
+    );
+    process.stdout.write(
+      `\r${(size / (1024 * 1024)).toFixed(
+        2
+      )} MB of data was sent. Total elapsed time is ${elapsed / 1000} s`
+    );
+    ostream.write(chunk);
+  });
+  client.on("end", () => {
+    console.log(
+      `\nFinished getting file. speed was: ${(
+        size /
+        (1024 * 1024) /
+        (elapsed / 1000)
+      ).toFixed(2)} MB/s`
+    );
+    process.exit();
+  });
+
+  /* client.on("data", function (data) {
+    console.log("Received: " + data);
+    //client.destroy();
+    fs.appendFile(filePath, data, (err, data) => {
+      if (err) throw err;
+      console.log("escribe");
+    });
+  });
+
+  client.on("close", function () {
+    console.log("Connection closed");
+  }); */
+}
+
+module.exports = {
+  startDownload,
+};
